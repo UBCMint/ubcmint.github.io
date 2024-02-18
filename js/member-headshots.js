b8982393042258decc092bf9959f40003cf8714f
Megan Ong <meganong1@gmail.com>
2024-02-18 12:47:48 -0800

    update team page

--- js/member-headshots.js
+++ js/member-headshots.js
@@ -4,16 +4,8 @@ const memberHeadshots = {
         "Sze Lok Ng.jpg"
     ],
     "software": [
-        "Annie Wang.jpg",
-        "Brady Vanderzalm.jpg",
-        "Cher Peng.jpg" ,
         "David Kim.jpg",
-        "Jaiden Siu.jpg",
-        "Josiah Tsang.jpg",
-        "Matthew Kwong.jpg",
-        "Nick Vo.jpg",
         "Peter Na.jpg",
-        "Stephanie Quon.png",
         "Trevor Liu.jpg"
     ],
     "deep-learning": [
@@ -22,15 +14,7 @@ const memberHeadshots = {
         "Ore Adeniyi.jpg"
     ],
     "signal-processing": [
-        "Ava Adli.jpeg",
-        "Ben Magyar.jpg",
-        "Ceana Macatula.jpg",
-        "Chloe Christensen.jpeg",
-        "Eleni Kapetanaki.png",
-        "Eric Zhang.jpg",
-        "Faith Tan.jpg",
         "Faraneh Yahyaei-Moayyed.jpg",
-        "Gurnoor Kaur.png"
     ],
     "electrical": [
         "Abdel-Rahman AL Faouri.jpg",
@@ -55,11 +39,7 @@ const memberHeadshots = {
         "Wesley Lam.jpg",
         "William Zhang.jpg"
     ],
-    "mentees": [
-        "Ella Buric.jpg",
-        "Keefee Rahman.jpg",
-        "Nina Gliosca.png"
-    ]
+
 }
 
 export { memberHeadshots };
