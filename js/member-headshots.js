78c3d4f4b68e0f119c196e20039c776e01da60ed
Megan Ong <meganong1@gmail.com>
2024-10-07 17:48:27 -0700

    update about and project page

--- js/member-headshots.js
+++ js/member-headshots.js
@@ -1,45 +1,77 @@
 const memberHeadshots = {
-    "co-captains": [
-        "Priyanshu Mahey.jpg",
-        "Sze Lok Ng.jpg"
-    ],
-    "software": [
-        "David Kim.jpg",
-        "Peter Na.jpg",
-        "Trevor Liu.jpg"
+    "co-captains": ["Priyanshu Mahey.jpg", "Sze Lok Ng.jpg"],
+    software: [
+      "Albert Guo.png",
+      "Ali Abbas.png",
+      "Andrew Feng.png",
+      "Daniel Yoo.png",
+      "David Kim.jpg",
+      "Durukan Bütün.png",
+      "Hassan ElGhayaty.png",
+      "Jayden Piao.png",
+      "Jenny Sun.png",
+      "Karry Jiang.png",
+      "Megan Ong.png",
+      "Mona Abusharkh.png",
+      "Nima.png",
+      "Parth Sanan.png",
+      "Patrick Ly.png",
+      "Peter Lu.png",
+      "Peter Na.jpg",
+      "Rachel Chiu.png",
+      "Sohbat Sandhu.png",
+      "Tammy Kim.png",
+      "Trevor Liu.jpg",
     ],
     "deep-learning": [
-        "Eva Wang.jpeg",
-        "Joshua Park.jpg",
-        "Ore Adeniyi.jpg"
+      "Abhigyan Johori.png",
+      "Akira Kudo.png",
+      "Eva Wang.jpeg",
+      "Gyo-Jin Kang.png",
+      "Joshua Park.jpg",
+      "Jun He Cui.png",
+      "Ore Adeniyi.jpg",
+      "Sanidhya Singh.png"
     ],
-    "signal-processing": [
-        "Faraneh Yahyaei-Moayyed.jpg",
+    neuroscience: [
+      "Ashley Choo.png",
+      "Lavanya Shah.png",
+      "Samantha Chee.png",
+      "Tarini Rajasekar.png"
     ],
-    "electrical": [
-        "Abdel-Rahman AL Faouri.jpg",
-        "Cameron Fletcher.jpg",
-        "Emily Choi.jpg",
-        "Holly MacGillivray.png",
-        "Jaden Sequeira.png",
-        "Karina Kumar.jpg",
-        "Omar Asaker.png",
-        "Tara Rouhbakhsh.png",
-        "Udaykaran Madaan.png"
+    "signal-processing": ["Athina Law.png", "Owen Gill.png"],
+    electrical: [
+      "Abdel-Rahman AL Faouri.jpg",
+      "Cameron Fletcher.jpg",
+      "Emily Choi.jpg",
+      "Holly MacGillivray.png",
+      "Jaden Sequeira.png",
+      "Karina Kumar.jpg",
+      "Omar Asaker.png",
+      "Tara Rouhbakhsh.png",
+      "Udaykaran Madaan.png",
+      "Reuben Jose.png"
     ],
-    "mechanical": [
-        "Angela Li.png",
-        "Jack Plant.jpg",
-        "Jimmy Yang.jpg",
-        "Karina Kumar.jpg",
-        "Kriti Verma.jpeg",
-        "Sofia Cecic.jpg",
-        "Tiffany Huang.jpg",
-        "Victor Li.jpg",
-        "Wesley Lam.jpg",
-        "William Zhang.jpg"
+    mechanical: [
+      "Angela Li.png",
+      "Jack Plant.jpg",
+      "Jimmy Yang.jpg",
+      "Karina Kumar.jpg",
+      "Kriti Verma.jpeg",
+      "Sofia Cecic.jpg",
+      "Tiffany Huang.jpg",
+      "Victor Li.jpg",
+      "Wesley Lam.jpg",
+      "William Zhang.jpg",
     ],
-
-}
-
-export { memberHeadshots };
+    ux: [
+      "Angelina Makkar.png",
+      "Ella Lowe-Ng.png",
+      "Jessie Megan.png",
+      "Mariane Olivan.png",
+      "Sonja Deng.png"
+    ],
+  };
+  
+  export { memberHeadshots };
+  
